69f3f9763dd453cec33963dd4c641ee4678bc090
ZhangYukaiJim <yzhanghe@connect.ust.hk>
2020-10-01 14:50:11 +0800

    fix bug

--- train_mixup.py
+++ train_mixup.py
@@ -288,8 +288,10 @@ def eval_one_epoch(sess, ops, test_writer):
             end_idx = (batch_idx+1) * BATCH_SIZE
 
             feed_dict = {ops['pointclouds_pl']: current_data[start_idx:end_idx, :, :],
-                         ops['labels_pl']: current_label[start_idx:end_idx],
-                         ops['is_training_pl']: is_training}
+                         ops['labels_a_pl']: current_label[start_idx:end_idx],
+                         ops['labels_b_pl']: current_label[start_idx:end_idx],
+                         ops['is_training_pl']: is_training,
+                         ops['lam_pl']: 1}
             summary, step, loss_val, pred_val = sess.run([ops['merged'], ops['step'],
                 ops['loss'], ops['pred']], feed_dict=feed_dict)
             pred_val = np.argmax(pred_val, 1)
